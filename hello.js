c536f1e2e1730984f0a309443ce78d65559effc2
Harshbir Singh <singh.hveer1@gmail.com>
2022-01-10 00:59:44 -0700

    More updates

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
+//Example 1
 const sayHello = function (name) {
   console.log("Hello, " + name)
 }
@@ -5,3 +6,17 @@ const sayHello = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+// Example 2  (console.log & return)
+const sayHelloToConsole = function (name){
+  console.log("Hello, " + name);
+}
+sayHelloToConsole("John");
+
+const returnHello = function (name){
+  return "Hello, " + name;
+}
+
+const greeting = returnHello("John");
+console.log(greeting);
+
