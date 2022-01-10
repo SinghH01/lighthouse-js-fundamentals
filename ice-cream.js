18205a923590786d7ba99a1493a90fb943ccf28c
Harshbir Singh <singh.hveer1@gmail.com>
2022-01-10 14:23:55 -0700

    no changes

--- ice-cream.js
+++ ice-cream.js
@@ -4,6 +4,16 @@ console.log(iceCreamFlavours);
 
 // Your code below here...
 
-//ADD FLAVOR
+// Step - 1 Add flavor
 iceCreamFlavours.push("root beer");
 console.log(iceCreamFlavours);
+
+// Step - 2 Get the First Flavour
+console.log(iceCreamFlavours[0]);
+
+//Step - 3 Get the Last Flavour
+console.log(iceCreamFlavours[ iceCreamFlavours.length-1 ]);
+
+// Step - 4 Total Number of Flavours
+console.log(iceCreamFlavours.length);
+
